0a6980ecf04fee22315f32bcf1672456683d1214
zaid kamil <xaidmetamorphos@gmail.com>
2020-06-10 17:05:37 +0530

    decision tree and random forest example update

---
+++ template.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import os
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
